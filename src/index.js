f8285b200cad19dc191734dcef54c5cd37104aa7
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 17:14:27 -0300

    Include signin and signup views separated from "/" rooted routes

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import './index.css';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { SignIn } from './views/signup/SignIn';
+import { SignIn } from './views/signin/SignIn';
 import { Clients } from './views/client/Clients';
+import { SignUp } from './views/signup/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<BrowserRouter>
 		<Routes>
+			<Route path="/signin" element={<SignIn/>}/>
+			<Route path="/signup" element={<SignUp/>}/>
 			<Route path="/" element={<App />}>
-				<Route path="/signin" element={<SignIn/>}/>
 				<Route path="/clients" element={<Clients/>}/>
 				<Route path="*" element={
 					<main style={{ padding: "1rem" }}>
