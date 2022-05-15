9b513ba3a6e2b2a5509231db752b7935e23c9689
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 15:33:10 -0300

    Add navigation control and fix specifics problems in SignIn

--- src/views/signup/SignIn.js
+++ src/views/signup/SignIn.js
@@ -1,22 +1,28 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { BASE_API_URL } from "../../config";
 
 const http = axios.create({baseURL: BASE_API_URL});
 
 export function SignIn() {
-	let [email, setEmail] = useState();
-	let [password, setPassword] = useState();
+	let [email, setEmail] = useState("");
+	let [password, setPassword] = useState("");
+	let navigate = useNavigate();
 
-	let signin = useCallback(() => {
+	let signin = useCallback((e) => {
+		e.preventDefault();
 		http.post("/auth/signin", {username: email, password})
-			.then(({data}) => localStorage.setItem("accessToken", data.accessToken));
-	}, [email, password]);
+			.then(({data}) => {
+				localStorage.setItem("accessToken", data.accessToken)
+				navigate("/clients");
+			});
+	}, [email, password, navigate]);
 
 	return (
 		<form onSubmit={signin}>
-			<input className="d-block" value={email} onChange={setEmail}/>
-			<input className="d-block" value={password} onChange={setPassword}/>
+			<input className="d-block" value={email} onChange={(e) => setEmail(e.target.value)}/>
+			<input className="d-block" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
 			<button type="submit">Sign In</button>
 		</form>
 	);
