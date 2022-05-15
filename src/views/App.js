656ce7a45e4c23cfc082ab550abda5d5a7b017e5
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 15:32:06 -0300

    Check accessToken when route changes

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
+	let location = useLocation();
+	let navigate = useNavigate();
+
+	useEffect(() => {
+		if (!localStorage.getItem("accessToken"))
+			navigate("/signin");
+	}, [location, navigate]);
 
 	return (
 		<div className="App">
+			Welcome
 			<Outlet/>
 		</div>
 	);
