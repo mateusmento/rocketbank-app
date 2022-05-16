621d35ff3c2033ba16ff237b38a49737c2243aad
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-16 19:16:34 -0300

    Add validation to ClientRow and use formik

--- src/views/client/ClientRow.js
+++ src/views/client/ClientRow.js
@@ -1,35 +1,59 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { Fragment, useCallback, useState } from 'react';
 import { Editable, EditableContext } from '../../ui/Editable';
 
 export function ClientRow({client: originalClient, onRemove, onUpdate}) {
 	let [isEditing, setIsEditing] = useState(false);
 
-	let [client, setClient] = useState(() => clone(originalClient));
-
-	let setField = useCallback((name, value) => {
-		setClient({...client, [name]: value});
-	}, [client]);
+	let formik;
 
 	let startEditing = useCallback(() => {
 		setIsEditing(true);
 	}, []);
 
-	let confirmEditing = useCallback(() => {
-		setIsEditing(false);
-		onUpdate(diff(originalClient, client));
-	}, [originalClient, client, onUpdate]);
+	let confirmEditing = useCallback(async (client) => {
+		try {
+			await onUpdate(diff(originalClient, client));
+		} finally {
+			setIsEditing(false);
+			formik.setValues(client);
+		}
+	}, [originalClient, onUpdate, formik]);
 
 	let cancelEditing = useCallback(() => {
-		setClient(clone(originalClient));
 		setIsEditing(false);
-	}, [originalClient]);
+		formik.setValues(clone(originalClient));
+	}, [originalClient, formik]);
+
+	formik = useFormik({
+		initialValues: clone(originalClient),
+		onSubmit: confirmEditing,
+		validationSchema: Yup.object().shape({
+			name: Yup.string().required("Forneça o nome do cliente"),
+			cpf: Yup.string().required("Forneça o CPF do cliente"),
+			birthDate: Yup.date().required("Forneça a data de nascimento do cliente"),
+		})
+	});
 
 	return (
 		<tr>
 			<EditableContext.Provider value={isEditing}>
-				<td><Editable value={client.name} onChange={(e) => setField("name", e.target.value)}/></td>
-				<td><Editable value={client.cpf} onChange={(e) => setField("cpf", e.target.value)}/></td>
-				<td><Editable value={client.birthDate} onChange={(e) => setField("birthDate", e.target.value)}/></td>
+				<td>
+					<Editable value={formik.values.name} name="name" onChange={formik.handleChange}/>
+					{isEditing && formik.errors.name && formik.touched.name
+						&& <small className="d-block">{formik.errors.name}</small>}
+				</td>
+				<td>
+					<Editable value={formik.values.cpf} name="cpf" onChange={formik.handleChange}/>
+					{isEditing && formik.errors.cpf && formik.touched.cpf
+						&& <small className="d-block">{formik.errors.cpf}</small>}
+				</td>
+				<td>
+					<Editable value={formik.values.birthDate} name="birthDate" onChange={formik.handleChange}/>
+					{isEditing && formik.errors.birthDate && formik.touched.birthDate
+						&& <small className="d-block">{formik.errors.birthDate}</small>}
+				</td>
 			</EditableContext.Provider>
 			<td>
 				{!isEditing ?
@@ -39,8 +63,8 @@ export function ClientRow({client: originalClient, onRemove, onUpdate}) {
 						</Fragment>
 					:
 						<Fragment>
-							<button onClick={confirmEditing}>Atualizar</button>
-							<button onClick={cancelEditing}>Cancelar</button>
+							<button type="button" onClick={formik.handleSubmit}>Atualizar</button>
+							<button type="button" onClick={cancelEditing}>Cancelar</button>
 						</Fragment>
 				}
 			</td>
