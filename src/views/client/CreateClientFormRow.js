c7518a5fecc814eaf457933efa74f2c644089dc4
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-15 21:52:27 -0300

    Format CreateClientFormRow

--- src/views/client/CreateClientFormRow.js
+++ src/views/client/CreateClientFormRow.js
@@ -9,9 +9,9 @@ export function CreateClientFormRow({onCreate}) {
 
 	return (
 		<tr>
-				<td><input value={client.name} onChange={(e) => setField("name", e.target.value)}/></td>
-				<td><input value={client.cpf} onChange={(e) => setField("cpf", e.target.value)}/></td>
-				<td><input value={client.birthDate} onChange={(e) => setField("birthDate", e.target.value)}/></td>
+			<td><input value={client.name} onChange={(e) => setField("name", e.target.value)}/></td>
+			<td><input value={client.cpf} onChange={(e) => setField("cpf", e.target.value)}/></td>
+			<td><input value={client.birthDate} onChange={(e) => setField("birthDate", e.target.value)}/></td>
 			<td><button onClick={() => onCreate(client)}>Criar</button></td>
 		</tr>
 	);
