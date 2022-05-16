4cbd9f732b73b9d1247af5376fb54842428b17da
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-16 19:15:29 -0300

    Use formik and add validation in CreateClientFormRow

--- src/views/client/CreateClientFormRow.js
+++ src/views/client/CreateClientFormRow.js
@@ -1,18 +1,38 @@
-import { useCallback, useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export function CreateClientFormRow({onCreate}) {
-	let [client, setClient] = useState({});
 
-	let setField = useCallback((name, value) => {
-		setClient({...client, [name]: value});
-	}, [client]);
+	let formik = useFormik({
+		initialValues: {},
+		onSubmit: onCreate,
+		validationSchema: Yup.object().shape({
+			name: Yup.string().required("Forneça o nome do cliente"),
+			cpf: Yup.string().required("Forneça o CPF do cliente"),
+			birthDate: Yup.date().required("Forneça a data de nascimento do cliente"),
+		})
+	});
 
 	return (
 		<tr>
-			<td><input value={client.name} onChange={(e) => setField("name", e.target.value)}/></td>
-			<td><input value={client.cpf} onChange={(e) => setField("cpf", e.target.value)}/></td>
-			<td><input value={client.birthDate} onChange={(e) => setField("birthDate", e.target.value)}/></td>
-			<td><button onClick={() => onCreate(client)}>Criar</button></td>
+			<td>
+				<input value={formik.values.name} name="name" onChange={formik.handleChange}/>
+				{formik.errors.name && formik.touched.name
+						&& <small className="d-block">{formik.errors.name}</small>}
+			</td>
+			<td>
+				<input value={formik.values.cpf} name="cpf" onChange={formik.handleChange}/>
+				{formik.errors.cpf && formik.touched.cpf
+						&& <small className="d-block">{formik.errors.cpf}</small>}
+			</td>
+			<td>
+				<input value={formik.values.birthDate} name="birthDate" onChange={formik.handleChange}/>
+				{formik.errors.birthDate && formik.touched.birthDate
+						&& <small className="d-block">{formik.errors.birthDate}</small>}
+			</td>
+			<td>
+				<button type="button" onClick={formik.handleSubmit}>Criar</button>
+			</td>
 		</tr>
 	);
 }
