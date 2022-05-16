8eec9806e1b8eb16c13d08401f0a0d5abd1513c0
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-16 19:15:03 -0300

    Refactor App including Home as route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SignIn } from './views/signin/SignIn';
 import { Clients } from './views/client/Clients';
 import { SignUp } from './views/signup/SignUp';
+import { useCallback, useEffect } from 'react';
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { Home } from './views/home/Home';
+import jwtDecode  from 'jwt-decode';
 
 export function App() {
 
@@ -11,7 +14,8 @@ export function App() {
 			<Routes>
 				<Route path="/signin" element={<SignIn/>}/>
 				<Route path="/signup" element={<SignUp/>}/>
-				<Route path="/" element={<Home/>}>
+				<Route element={<Root/>}>
+					<Route path="/" element={<Home/>}/>
 					<Route path="/clients" element={<Clients/>}/>
 					<Route path="*" element={
 						<main style={{ padding: "1rem" }}>
@@ -21,5 +25,35 @@ export function App() {
 				</Route>
 			</Routes>
 		</BrowserRouter>
+	);
+}
+
+export function Root() {
+	let location = useLocation();
+	let navigate = useNavigate();
+
+	let signout = useCallback((e) => {
+		localStorage.removeItem("accessToken");
+		navigate("/signin")
+	}, [navigate]);
+
+	let verifyAccessToken = useCallback((token) => {
+		if (!token) navigate("/signin");
+		let signoutTimeout = token.exp * 1000 - new Date().getTime();
+		setTimeout(signout, signoutTimeout);
+	}, [navigate, signout]);
+
+	useEffect(() => {
+		let token = localStorage.getItem("accessToken");
+		verifyAccessToken(token && jwtDecode(token))
+	}, [location, navigate, verifyAccessToken]);
+
+	return (
+		<div className="App">
+			| <Link to="/">Home</Link>
+			| <Link to="/clients">Clientes</Link>
+			| <span onClick={signout}>Sair</span>
+			<Outlet/>
+		</div>
 	);
 }
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,27 +1,8 @@
-import { useCallback, useEffect } from 'react';
-import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
-
 export function Home() {
-	let location = useLocation();
-	let navigate = useNavigate();
-
-	useEffect(() => {
-		if (!localStorage.getItem("accessToken"))
-			navigate("/signin");
-	}, [location, navigate]);
-
-	let signout = useCallback((e) => {
-		localStorage.removeItem("accessToken");
-		navigate("/signin")
-	}, [navigate]);
-
 	return (
-		<div className="App">
-			Bem vindo
-			| <Link to="/">Home</Link>
-			| <Link to="/clients">Clientes</Link>
-			| <span onClick={signout}>Sair</span>
-			<Outlet/>
+		<div className="Home">
+			<img src="/logo.jpg" alt="Rocket Bank Logo"/>
+			<h2>Bem vindo</h2>
 		</div>
 	);
 }
