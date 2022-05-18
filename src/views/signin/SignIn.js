0a213969d2756c57dce482ba170d865063b7280a
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-18 12:12:45 -0300

    Adde validation for signin form

--- src/views/signin/SignIn.js
+++ src/views/signin/SignIn.js
@@ -6,6 +6,8 @@ import TextField from '@mui/material/TextField';
 import Grid from "@mui/material/Grid";
 import { Box, Card, Tab, Tabs, Typography } from "@mui/material";
 import { SignUp } from "../signup/SignUp";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
 export function SignIn() {
 	let [currentTab, setCurrentTab] = useState("signin");
@@ -17,41 +19,41 @@ export function SignIn() {
 	}, []);
 
 	return (
-		<Box>
-			<Grid container>
-				<Grid item container xs={7} sx={{paddingTop: 25, paddingLeft: 8}}>
-					<Grid item xs={3}>
-						<img src="/logo.png" alt="Rocket Bank Logo" style={{width: "100%", marginTop: -25}}/>
-					</Grid>
-					<Grid item xs={9}>
-						<Typography variant="h1" component="h2" color="#eee">
-							Rocketbank
-						</Typography>
-						<Typography variant="h5" component="h5" color="#ddd">
-							Transformação Digital ao alcance da sua empresa
-						</Typography>
-					</Grid>
+	<Box bgcolor="rgb(0, 166, 255)" width="100%" position="absolute" top="0" bottom="0">
+		<Grid container>
+			<Grid item container xs={7} sx={{paddingTop: 30, paddingLeft: 5}}>
+				<Grid item xs={3}>
+					<img src="/logo.png" alt="Rocket Bank Logo" style={{width: "100%", marginTop: -25}}/>
 				</Grid>
-				<Grid item xs={5}>
-					<Card sx={{minWidth: 300, marginTop: 18, marginRight: 10}}>
-						<Tabs value={currentTab} onChange={(e, tab) => setCurrentTab(tab)}>
-							<Tab label="Entrar" value="signin" {...a11yProps(1)} sx={{flex: 1}}/>
-							<Tab label="Criar Conta" value="signup" {...a11yProps(2)} sx={{flex: 1}}/>
-						</Tabs>
-						<TabPanel value={currentTab} index="signin">
-							<Box sx={{paddingY: 10, paddingX: 15}}>
-								<SignInForm email={email}/>
-							</Box>
-						</TabPanel>
-						<TabPanel value={currentTab} index="signup">
-							<Box sx={{paddingY: 6, paddingX: 15}}>
-								<SignUp onSignUp={onSignUp}/>
-							</Box>
-						</TabPanel>
-					</Card>
+				<Grid item xs={9}>
+					<Typography variant="h1" component="h2" color="#eee">
+						Rocketbank
+					</Typography>
+					<Typography variant="h5" component="h5" color="#ddd">
+						Transformação Digital ao alcance da sua empresa
+					</Typography>
 				</Grid>
 			</Grid>
-		</Box>
+			<Grid item xs={5}>
+				<Card sx={{minWidth: 300, marginTop: 18, marginRight: 10}}>
+					<Tabs value={currentTab} onChange={(e, tab) => setCurrentTab(tab)}>
+						<Tab label="Entrar" value="signin" {...a11yProps(1)} sx={{flex: 1}}/>
+						<Tab label="Criar Conta" value="signup" {...a11yProps(2)} sx={{flex: 1}}/>
+					</Tabs>
+					<TabPanel value={currentTab} index="signin">
+						<Box sx={{paddingY: 10, paddingX: 15}}>
+							<SignInForm email={email}/>
+						</Box>
+					</TabPanel>
+					<TabPanel value={currentTab} index="signup">
+						<Box sx={{paddingY: 6, paddingX: 15}}>
+							<SignUp onSignUp={onSignUp}/>
+						</Box>
+					</TabPanel>
+				</Card>
+			</Grid>
+		</Grid>
+	</Box>
 	);
 }
 
@@ -72,23 +74,34 @@ function a11yProps(index) {
 
 
 function SignInForm({email: receivedEmail}) {
-	let [email, setEmail] = useState(receivedEmail);
-	let [password, setPassword] = useState("");
 	let navigate = useNavigate();
 
-	useEffect(() => setEmail(receivedEmail), [receivedEmail]);
-
-	let signin = useCallback((e) => {
-		e.preventDefault();
-		http().post("/auth/signin", {email, password})
+	let signin = useCallback((credentials) => {
+		http().post("/auth/signin", credentials)
 			.then(({data}) => {
 				localStorage.setItem("accessToken", data.accessToken)
-				navigate("/clients");
+				navigate("/");
 			});
-	}, [email, password, navigate]);
+	}, [navigate]);
+
+	let formik = useFormik({
+		initialValues: {},
+		validateOnChange: true,
+		onSubmit: signin,
+		validationSchema: Yup.object().shape({
+			email: Yup.string()
+				.email("Email inválido")
+				.required("Forneça seu email"),
+			password: Yup.string().required("Forneça sua senha")
+		}),
+	});
+
+	useEffect(() => {
+		formik.setFieldValue("email", receivedEmail);
+	}, [receivedEmail]);
 
 	return (
-		<form onSubmit={signin}>
+		<form onSubmit={formik.handleSubmit}>
 			<Typography variant="h5" element="h3" marginLeft={3}>Entre com sua conta</Typography>
 			<TextField
 				label="Email"
@@ -96,8 +109,11 @@ function SignInForm({email: receivedEmail}) {
 				size="small"
 				fullWidth
 				margin="normal"
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
+				value={formik.values.email}
+				onChange={formik.handleChange}
+				error={formik.errors.email && formik.submitCount > 0}
+				helperText={formik.submitCount > 0 && formik.errors.email}
+				{...formik.getFieldProps("email")}
 			/>
 			<TextField
 				type="password"
@@ -106,9 +122,12 @@ function SignInForm({email: receivedEmail}) {
 				size="small"
 				fullWidth
 				margin="normal"
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
+				value={formik.values.password}
+				onChange={formik.handleChange}
+				error={formik.errors.password && formik.submitCount > 0}
+				helperText={formik.submitCount > 0 && formik.errors.password}
+				{...formik.getFieldProps("password")}
+				/>
 			<Button
 				type="submit"
 				variant="contained"
