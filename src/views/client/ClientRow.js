2bbdea5ea9ee4c0dac906fc93746c3bb6dc394fe
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-18 12:14:23 -0300

    Use material ui table components and format data field

--- src/views/client/ClientRow.js
+++ src/views/client/ClientRow.js
@@ -1,12 +1,20 @@
 import { useFormik } from 'formik';
-import * as Yup from 'yup';
 import { Fragment, useCallback, useState } from 'react';
 import { Editable, EditableContext } from '../../ui/Editable';
+import { Button, TableCell, TableRow } from '@mui/material';
+import { Edit, Delete, Check, Cancel } from '@mui/icons-material';
+import moment from 'moment';
+import { clientValidation } from './client-validation';
 
 export function ClientRow({client: originalClient, onRemove, onUpdate}) {
 	let [isEditing, setIsEditing] = useState(false);
 
 	let formik;
+
+	let formattedOriginalClient = useCallback(() => ({
+		...originalClient,
+		birthDate: moment(originalClient.birthDate, "YYYY-MM-DD").format("DD/MM/YYYY"),
+	}), [originalClient]);
 
 	let startEditing = useCallback(() => {
 		setIsEditing(true);
@@ -14,66 +22,61 @@ export function ClientRow({client: originalClient, onRemove, onUpdate}) {
 
 	let confirmEditing = useCallback(async (client) => {
 		try {
-			await onUpdate(diff(originalClient, client));
+			console.log(formattedOriginalClient(), client);
+			let patch = diff(formattedOriginalClient(), client);
+			if (patch.birthDate)
+				patch.birthDate = moment(patch.birthDate, "DD/MM/YYYY").format("YYYY-MM-DD");
+			await onUpdate(patch);
 		} finally {
 			setIsEditing(false);
 			formik.setValues(client);
 		}
-	}, [originalClient, onUpdate, formik]);
+	}, [formattedOriginalClient, onUpdate, formik]);
 
 	let cancelEditing = useCallback(() => {
 		setIsEditing(false);
-		formik.setValues(clone(originalClient));
-	}, [originalClient, formik]);
+		formik.setValues(formattedOriginalClient());
+	}, [formattedOriginalClient, formik]);
 
 	formik = useFormik({
-		initialValues: clone(originalClient),
+		initialValues: formattedOriginalClient(),
 		onSubmit: confirmEditing,
-		validationSchema: Yup.object().shape({
-			name: Yup.string().required("Forneça o nome do cliente"),
-			cpf: Yup.string().required("Forneça o CPF do cliente"),
-			birthDate: Yup.date().required("Forneça a data de nascimento do cliente"),
-		})
+		validationSchema: clientValidation
 	});
 
 	return (
-		<tr>
-			<EditableContext.Provider value={isEditing}>
-				<td>
-					<Editable value={formik.values.name} name="name" onChange={formik.handleChange}/>
-					{isEditing && formik.errors.name && formik.touched.name
-						&& <small className="d-block">{formik.errors.name}</small>}
-				</td>
-				<td>
-					<Editable value={formik.values.cpf} name="cpf" onChange={formik.handleChange}/>
-					{isEditing && formik.errors.cpf && formik.touched.cpf
-						&& <small className="d-block">{formik.errors.cpf}</small>}
-				</td>
-				<td>
-					<Editable value={formik.values.birthDate} name="birthDate" onChange={formik.handleChange}/>
-					{isEditing && formik.errors.birthDate && formik.touched.birthDate
-						&& <small className="d-block">{formik.errors.birthDate}</small>}
-				</td>
-			</EditableContext.Provider>
-			<td>
+		<TableRow>
+			<TableCell>
+				<form id="new-client-form" onSubmit={formik.handleSubmit}></form>
+				<Editable isEditing={isEditing} form="new-client-form" value={formik.values.name} name="name" onChange={formik.handleChange}/>
+				{isEditing && formik.errors.name && formik.touched.name
+					&& <small className="d-block">{formik.errors.name}</small>}
+			</TableCell>
+			<TableCell>
+				<Editable isEditing={isEditing} form="new-client-form" value={formik.values.cpf} name="cpf" onChange={formik.handleChange}/>
+				{isEditing && formik.errors.cpf && formik.touched.cpf
+					&& <small className="d-block">{formik.errors.cpf}</small>}
+			</TableCell>
+			<TableCell>
+				<Editable isEditing={isEditing} form="new-client-form" value={formik.values.birthDate} name="birthDate" onChange={formik.handleChange}/>
+				{isEditing && formik.errors.birthDate && formik.touched.birthDate
+					&& <small className="d-block">{formik.errors.birthDate}</small>}
+			</TableCell>
+			<TableCell>
 				{!isEditing ?
 						<Fragment>
-							<button onClick={startEditing}>Editar</button>
-							<button onClick={onRemove}>Excluir</button>
+							<Button type="button" onClick={startEditing}><Edit/></Button>
+							<Button type="button" onClick={onRemove}><Delete/></Button>
 						</Fragment>
 					:
 						<Fragment>
-							<button type="button" onClick={formik.handleSubmit}>Atualizar</button>
-							<button type="button" onClick={cancelEditing}>Cancelar</button>
+							<Button type="button" onClick={cancelEditing}><Cancel/></Button>
+							<Button form="new-client-form" type="submit"><Check/></Button>
 						</Fragment>
 				}
-			</td>
-		</tr>
+			</TableCell>
+		</TableRow>
 	);
-}
-
-function clone(value) {
-	return JSON.parse(JSON.stringify(value));
 }
 
 function diff(original, modified) {
