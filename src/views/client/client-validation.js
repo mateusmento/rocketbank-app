981245aec7b57eaf6d1de56bb8c593838d8310ba
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-18 12:48:07 -0300

    Fix cpf validation error

--- src/views/client/client-validation.js
+++ src/views/client/client-validation.js
@@ -6,7 +6,7 @@ export const clientValidation = Yup.object().shape({
 	name: Yup.string().required("Forneça o nome do cliente"),
 	cpf: Yup.string()
 		.required("Forneça o CPF do cliente")
-		.test("cpf", "CPF inválido", (value) => cpf.validate(value)),
+		.test("cpf", "CPF inválido", (value) => cpf.validate(value || '')),
 	birthDate: Yup.string()
 		.required("Forneça a data de nascimento do cliente")
 		.test("date", "Data inválida", (value) => moment(value, "DD/MM/YYYY").isValid()),
