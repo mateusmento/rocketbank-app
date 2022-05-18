7806b030a4cc47b55b76c75cf784ea8d08065e1b
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-18 12:11:57 -0300

    Add client validation schema

---
+++ src/views/client/client-validation.js
@@ -0,0 +1,13 @@
+import * as Yup from 'yup';
+import { cpf } from 'br-validations';
+import moment from 'moment';
+
+export const clientValidation = Yup.object().shape({
+	name: Yup.string().required("Forneça o nome do cliente"),
+	cpf: Yup.string()
+		.required("Forneça o CPF do cliente")
+		.test("cpf", "CPF inválido", (value) => cpf.validate(value)),
+	birthDate: Yup.string()
+		.required("Forneça a data de nascimento do cliente")
+		.test("date", "Data inválida", (value) => moment(value, "DD/MM/YYYY").isValid()),
+});
