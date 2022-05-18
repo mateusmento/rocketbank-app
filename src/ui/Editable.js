a907ce9b97b5a80fb1a43315deb9bf0fc910dadc
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-05-18 12:15:24 -0300

    Replace div contentediable with plain input element

--- src/ui/Editable.js
+++ src/ui/Editable.js
@@ -1,20 +1,17 @@
-import { createContext, useContext, useRef } from "react";
+import { createContext, useContext } from "react";
 import classname from 'classname';
 import './Editable.css';
 
 export const EditableContext = createContext(false);
 
-export function Editable({value, onChange, ...props}) {
-	let isEditing = useContext(EditableContext);
-	let divRef = useRef();
+export function Editable({isEditing, ...props}) {
+	let context = useContext(EditableContext);
+	isEditing = isEditing || context;
 	return (
-		<>
-			<div
-				className={classname("editable", {"not-editable": !isEditing})}
-				onKeyUp={(e) => onChange(e.target.innerText)}
-				contentEditable={isEditing}
-				ref={divRef} dangerouslySetInnerHTML={{__html: value}}/>
-			<input type="hidden" value={value} {...props}/>
-		</>
+		<input
+			className={classname("editable", {"not-editable": !isEditing})}
+			{...props}
+			readOnly={!isEditing}
+		/>
 	);
 }
